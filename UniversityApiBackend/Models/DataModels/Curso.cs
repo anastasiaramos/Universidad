eb6441dac317b5f781ad9ec05130105540bc3e1a
Anastasia Ramos <anastasia.ramosmayor@gmail.com>
2022-11-02 14:34:40 +0000

    Ejercicio 1 Curso de APIs con .NET

---
+++ UniversityApiBackend/Models/DataModels/Curso.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public enum NivelCurso { Basico, Intermedio, Avanzado}
+    public class Curso : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [Required, StringLength(280)]
+        public string DescripcionCorta { get; set; } = string.Empty;
+        [Required]
+        public string DescripcionLarga { get; set; } = string.Empty;
+        [Required]
+        public string PublicoObjetivo { get; set; } = string.Empty;
+        [Required]
+        public string Objetivos { get; set; } = string.Empty;
+        [Required]
+        public string Requisitos { get; set; } = string.Empty;
+        [Required]
+        public NivelCurso Nivel { get; set; }
+
+
+    }
+}
