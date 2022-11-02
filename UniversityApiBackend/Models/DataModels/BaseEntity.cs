bffc1f0f340167ab95d5c008443ec6bb1b3a6341
Anastasia Ramos <anastasia.ramosmayor@gmail.com>
2022-11-02 20:37:09 +0000

    Ejercicio 2

--- UniversityApiBackend/Models/DataModels/BaseEntity.cs
+++ UniversityApiBackend/Models/DataModels/BaseEntity.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace UniversityApiBackend.Models.DataModels
 {
@@ -7,10 +8,19 @@ namespace UniversityApiBackend.Models.DataModels
         [Required]
         [Key]
         public int Id { get; set; }
+        public int CreatorUserId { get; set; }
+        //[NotMapped]
+        //public virtual BaseEntity? CreatorUser { get; set; } = new BaseEntity();
         public string CreatedBy { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public int UpdaterUserId { get; set; }
+        //[NotMapped]
+        //public virtual BaseEntity? UpdaterUser { get; set; } = new BaseEntity();
         public string UpdatedBy { get; set; } = string.Empty;
         public DateTime? UpdatedAt { get; set; }
+        public int RemoverUserId { get; set; }
+        //[NotMapped]
+        //public virtual BaseEntity? RemoverUser { get; set; } = new BaseEntity();
         public string DeletedBy { get; set; } = string.Empty;
         public DateTime? DeletedAt { get; set; }
         public bool IsDeleted { get; set; } = false;
